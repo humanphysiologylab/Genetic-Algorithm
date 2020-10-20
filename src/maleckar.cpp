085f630211c32efa5cc3a72925b8360ce39925df
Alexander Timofeev <foo@bar.com>
2020-10-20 20:20:28 +0300

    small ap function fix

--- src/maleckar.cpp
+++ src/maleckar.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <cassert>
 #include "maleckar.h"
 
 
@@ -443,7 +443,7 @@ void euler(double dt, State *S, State *R) {
 
 
 int action_potential(struct State *initial_state, double *scaling_coefficients, double *AP, float CL, float amp,
-                     int AP_length, int iso, int baseline_index, int amount_of_baselines, int amount_of_genes) {
+                     const int AP_length, int iso, int baseline_index, int amount_of_baselines, int amount_of_genes) {
 
     const int chain_length = 1; //?? maybe it should not be hardcoded 
     const int target_cell_index = chain_length / 2;
@@ -477,13 +477,10 @@ int action_potential(struct State *initial_state, double *scaling_coefficients, 
     int AP_length_current = 0, dt_counter = 0;
 
     while (t <= ft) {
-
-        if (t >= ft - CL && t < ft - CL + AP_length) {
-            if (dt_counter % skip == 0) {
-                if (AP_length_current < AP_length) {
-                    AP[AP_length_current] = S[target_cell_index].V; //?? maybe rewrite because of lots of LL cache write misses
-                }
-                AP_length_current += 1;
+        if (t >= ft - CL) {
+            if (dt_counter % skip == 0 && AP_length_current < AP_length) {
+                AP[AP_length_current] = S[target_cell_index].V;
+                AP_length_current++;
             }
             dt_counter++;
         }
@@ -509,7 +506,8 @@ int action_potential(struct State *initial_state, double *scaling_coefficients, 
         }
         t += dt;
     }
-
+    assert(AP_length_current == AP_length);
+    
     //Rewriting states!
     scaling_coefficients[genes_without_concentrations + baseline_index] = S[target_cell_index].Na_i;
     scaling_coefficients[genes_without_concentrations + amount_of_baselines + baseline_index] = S[target_cell_index].Ca_rel;
